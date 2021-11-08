b8dc56e63d301205214fd96bf22d5b17c5e3713e
sg0831 <dev.sg0831@gmail.com>
2021-11-09 00:56:49 +0900

    setup front

--- frontend/relay_writer.js
+++ frontend/relay_writer.js
@@ -1,11 +1,13 @@
-$(document).ready( function() {
-  $.ajax({
-    url: "hppt://localhost:8000/api/user",
-    type: "GET",
-    dataType: "json",
-    success: function(res) {
-      alert("ajax 성공!");
-      // $("#backend-test").append(res);
-    }
-  })
-})
+// $(document).ready( function() {
+//   $.ajax({
+//     url: "http://152.70.251.145:8000/api/user/",
+//     type: "GET",
+//     dataType: "json",
+//     success: function(res) {
+//       alert("ajax 성공! value: " + res[0].username);
+//     },
+//     error : function(XMLHttpRequest, textStatus, errorThrown){ // 비동기 통신이 실패할경우 error 콜백으로 들어옵니다.
+//       alert("통신 실패.")
+//     }
+//   })
+// })
