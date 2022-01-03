71c554fcf19e1e32fb3afdff204638371fba699c
sg0831 <dev.sg0831@gmail.com>
2022-01-03 23:06:32 +0900

    Finish clean all errors

--- frontend/relay_writer/src/components/home/RecentList.js
+++ frontend/relay_writer/src/components/home/RecentList.js
@@ -5,148 +5,45 @@ import thumbnail from './logo_image.jpg';
 
 
 export default function RecentList() {
+  const post_list = [];
+  for (let i = 0; i < 10; i++) {
+    post_list.push({
+      title: '라이언 이야기',
+      genre: '데일리',
+      views: 0,
+      recommends: 0,
+      hash_tags: ['#일상', '#데일리', '#학교', '#감성', '#진지']
+    });
+  }
+
   return (
     <div>
       {/* 최근 글 목록 */}
       <h2 className={styles.title_recent_list}>최근 글 목록</h2>
       <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
+        {post_list.map((post, index) => (
+          <section className={styles.recent_list_content}>
+            <section className={styles.thumbnail}>
+              <img src={thumbnail} alt='Thumbnail' />
+            </section>
+            <section className={styles.impo}>
+              <Link className={styles.impo_title} to="/postDetail/1">{post.title}</Link>
+              <span className={styles.impo_genre}>[{post.genre}]</span>
+              <span className={styles.impo_views}>조회수: {post.views}</span>
+              <span className={styles.impo_recommends}>추천수: {post.recommends}</span>
 
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
+              <section className={styles.hash_tags}>
+                {post.hash_tags.map((tag, index) => (
+                  <span className={styles.hash_tag}>{tag}</span>
+                ))}
+              </section>
+            </section>
 
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
+            <section className={styles.more_button}>
+              <button>More</button>
+            </section>
           </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
-
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
-
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
-
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
-
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
-
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
-
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
-      </article>
-
-      <article className={styles.recent_list}>
-        <section className={styles.recent_list_content}>
-          <section className={styles.thumbnail}>
-            <img src={thumbnail} alt='Thumbnail' />
-          </section>
-          <section className={styles.impo}>
-            <Link className={styles.impo_title} to="/postDetail/1">라이언 이야기</Link>
-            <span className={styles.impo_genre}>[데일리]</span>
-            <span className={styles.impo_views}>조회수: </span>
-            <span className={styles.impo_recommends}>추천수: </span>
-          </section>
-        </section>
+        ))}
       </article>
     </div>
   )
--- frontend/relay_writer/src/components/home/TodayTop10.js
+++ frontend/relay_writer/src/components/home/TodayTop10.js
@@ -1,138 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './TodayTop10.module.css';
-import thumbnail from './test_image.jpg';
+import thumbnail from './logo_image.jpg';
 
 
 export default function TodayTop10() {
+  const post_list = [];
+  for (let i = 0; i < 5; i++) {
+    post_list.push({
+      title: '라이언 이야기',
+      genre: '데일리',
+      views: 0,
+      recommends: 0
+    });
+  }
   return (
     <div>
-              <h2 className={ styles.title }>Today's T.O.P 10</h2>
-        <article className={styles.top10}>
-          <section className={styles.top_list}>
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='썸네일' />
+      <h2 className={styles.title}>Today's T.O.P 10</h2>
+      <article className={styles.top10}>
+        {/* 왼쪽 */}
+        <section className={styles.top_list} >
+          {post_list.map((post, index) => (
+            <section className={styles.top_list_content}>
+              <section className={styles.thumbnail}>
+                <img src={thumbnail} alt='썸네일' />
               </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
+              <section className={styles.impo}>
+                <Link className={styles.impo_title} to="/postDetail/1">{post.title}</Link>
+                <span className={styles.impo_genre}>[{post.genre}]</span>
+                <span className={styles.impo_views}>조회수: {post.views}</span>
+                <span className={styles.impo_recommends}>추천수: {post.recommends}</span>
               </section>
             </section>
+          ))}
+        </section>
 
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
+        {/* 오른쪽 */}
+        <section className={styles.top_list} >
+          {post_list.map((post, index) => (
+            <section className={styles.top_list_content}>
+              <section className={styles.thumbnail}>
+                <img src={thumbnail} alt='썸네일' />
               </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
+              <section className={styles.impo}>
+                <Link className={styles.impo_title} to="/postDetail/1">{post.title}</Link>
+                <span className={styles.impo_genre}>[{post.genre}]</span>
+                <span className={styles.impo_views}>조회수: {post.views}</span>
+                <span className={styles.impo_recommends}>추천수: {post.recommends}</span>
               </section>
             </section>
-
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-          </section>
-
-          <section className={styles.top_list}>
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-
-            <section className={ styles.top_list_content }>
-              <section className={ styles.thumbnail }>
-                <img src={ thumbnail } alt='Thumbnail' />
-              </section>
-              <section className={ styles.impo }>
-                <Link className={ styles.impo_title } to="/postDetail/1">라이언 이야기</Link>
-                <span className={ styles.impo_genre }>[데일리]</span>
-                <span className={ styles.impo_views }>조회수: </span>
-                <span className={ styles.impo_recommends }>추천수: </span>
-              </section>
-            </section>
-          </section>
-        </article>
-    </div>
+          ))}
+        </section>
+      </article >
+    </div >
   )
 }
