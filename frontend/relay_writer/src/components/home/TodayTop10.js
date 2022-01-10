cbd7e5ae8383a2d94b8b9b74147af5cee7c208a2
sg0831 <dev.sg0831@gmail.com>
2022-01-11 07:17:07 +0900

    Try make today's top10 and recent_list from backend API

--- frontend/relay_writer/src/components/home/TodayTop10.js
+++ frontend/relay_writer/src/components/home/TodayTop10.js
@@ -1,32 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './TodayTop10.module.css';
-import thumbnail from './logo_image.jpg';
 
 
-export default function TodayTop10() {
-  const url = "localhost:8000/api/user";
-  let [post_list, setPost_list ] = useState();
-  let left_list = [];
-  let right_list = [];
-
-  useEffect( (post_list) => {
-    // get 요청
-    async function get() {
-      let result = await axios.get(url);
-      setPost_list( result );
-      alert( post_list );
-      // get 응답이 완료된 후 실행
-    }
-    
-    get();
-  }, [] );
-
+export default function TodayTop10(props) {
   return (
     <div>
-      <h2 className={styles.title}>Today's T.O.P 10</h2>
-    
-    </div >
+      <h2 className={styles.title}>Today's T.O.P 10 </h2>
+
+      <button onClick={ () => {
+          alert(`테스트: ${props.left_list.values}`);
+        } }>테스트 버튼</button>
+    </div>
   )
 }
